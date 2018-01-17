f3037cc07c4b7daf3b847bcf192cb23b4259aa61
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2018-01-16 21:39:02 -0500

    Add cond

--- src/cond.ts
+++ src/cond.ts
@@ -1,13 +1,16 @@
 import { Predicate, Lazy } from './types';
-import { ifElse } from './if-else';
+import { head, tail } from './array';
+import ifElse from './if-else';
 
 export type CondOption<T, U> = [Predicate<T>, Lazy<U>];
+export const predicate = <T>(option: CondOption<T, any>) => option[0];
+export const evaluation = <T>(option: CondOption<any, T>) => option[1];
 
-export default <T, U>(options: CondOption<T, U>[]) =>
-  (t: T) =>
-    ifElse(!options.length,
-      () => undefined,
-      () => ifElse());
+const cond = <T, U>(options: CondOption<T, U>[], t: T): U | undefined =>
+  ifElse(!options.length,
+    () => undefined,
+    () => ifElse(predicate(head(options))(t),
+      () => evaluation(head(options))(),
+      () => cond(tail(options), t)));
 
-
-
+export default cond;
--- test/cond.ts
+++ test/cond.ts
@@ -2,30 +2,30 @@ import cond from '../src/cond';
 
 describe('cond', () => {
   it('selects the first match', () => {
-    const switcher = ([
-      [(x: number) => x == 1, () => '1'],
+    const output = cond([
+      [x => x == 1, () => '1'],
       [x => x == 2, () => '2'],
       [x => x == 3, () => '3'],
       [x => x == 4, () => '444'],
       [x => x == 5, () => '5'],
       [x => x == 4, () => '4'],
       [x => x == 5, () => 9],
-    ]);
+    ], 4);
 
-    expect(switcher(4)).toEqual('444');
+    expect(output).toEqual('444');
   });
 
   it('returns undefined if there are no matches', () => {
-    const switcher = ([
-      [(x: number) => x == 1, () => '1'],
+    const output = cond([
+      [x => x == 1, () => '1'],
       [x => x == 2, () => '2'],
       [x => x == 3, () => '3'],
       [x => x == 4, () => '444'],
       [x => x == 5, () => '5'],
       [x => x == 4, () => '4'],
       [x => x == 5, () => 9],
-    ]);
+    ], 6);
 
-    expect(switcher(6)).toEqual(undefined);
+    expect(output).toEqual(undefined);
   });
 });
